1b267fa760d02c69e1d825eda8803e364b761833
Vu3u <vudubond@gmail.com>
2024-08-09 17:13:44 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
-var express=require('express');
-var app=express();
+// Load environment variables from .env file
+require('dotenv').config();
 
-var routes=require('./routes/route.js');
+var express = require('express');
+var app = express();
 
-app.set('view engine','ejs');
+var routes = require('./routes/route.js');
+
+app.set('view engine', 'ejs');
 
 app.use(express.static(__dirname + '/public'));
 
-app.get('/',routes.home);
+app.get('/', routes.home);
 
+// Use the PORT from the .env file or default to 3000
 var port = process.env.PORT || 3000;
 
-var server=app.listen(port,function(req,res){
-    console.log("Catch the action at http://localhost:"+port);
+var server = app.listen(port, function () {
+    console.log("Catch the action at http://localhost:" + port);
 });
